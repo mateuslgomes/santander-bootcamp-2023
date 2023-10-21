353d758d5c0b05f3d03bfc562f47c95c311e72f3
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-10-20 22:48:52 -0300

    Implementando camada de negócio

--- src/main/java/dio/me/repository/UserRepositorie.java
+++
@@ -1,8 +0,0 @@
-package dio.me.repository;
-
-import dio.me.domain.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepositorie extends JpaRepository<User, Long> {}
